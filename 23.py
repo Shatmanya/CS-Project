208d11f8c1534c7b6277e788b18ad771aa94c78a
Shatmanaya <sanjaykmr157@outlook.com>
2021-01-08 12:11:49 +0530

    added 23

--- 23.py
+++ 23.py
@@ -1,1 +1,11 @@
-# Program to input an list and arrange all numbers in descending
+# Program to input an list and arrange all numbers in descending order
+
+list = []
+number = int(input("enter total number of items "))
+for i in range(0,number):
+    element = int(input())
+    list.append(element)
+print(list)
+
+list.sort(reverse=True)
+print( list )
