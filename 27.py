c1a97ca693884bd3e02fc314511ed47e11549b55
Shatmanaya <sanjaykmr157@outlook.com>
2021-01-08 12:54:52 +0530

    renamed:    25.py -> 26.py 	new file:   27.py

---
+++ 27.py
@@ -0,0 +1,8 @@
+#Program using for loop,to print out the decimal equivalent of 1/2,1/3,1/4,....1/10
+
+decimal = float()
+print("decimals")
+for i in range(2,11):
+    decimal = 1/i
+    print(decimal)
+     
