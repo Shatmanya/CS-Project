5bc38f8863020c2ee09613dac436f99801f98e6d
Shatmanaya <sanjaykmr157@outlook.com>
2021-01-10 23:42:40 +0530

    solved 32

---
+++ 32.py
@@ -0,0 +1,22 @@
+# Program to print upper triangle matrix
+
+rows = int(input("Enter the Number of rows : " ))
+column = int(input("Enter the Number of Columns: "))
+
+print("Enter the elements of First Matrix:")
+matrix= [[int(input()) for i in range(column)] for i in range(rows)]
+print("First Matrix is: ")
+for n in matrix:
+    print(n)
+
+for i in range(rows):
+    for j in range(column):
+        if(i<=j):
+            print(matrix[i][j],end="\t")
+        else:
+            print(' ',end="\t")
+    print()
+
+
+
+
