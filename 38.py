5be6ba86363f43f8c5d3ee943d665833a73d0786
Shatmanaya <sanjaykmr157@outlook.com>
2021-01-11 11:13:12 +0530

    solved 38

---
+++ 38.py
@@ -0,0 +1,15 @@
+t1 = tuple( )  
+n = input("Total number of values m first tuple") 
+for i in range (n):  
+    a = input("Enter elements")  
+t2 = t2 + (a, ) 
+print ("First tuple")  
+print (t1) 
+print("Second tuple")  
+print( t2 ) 
+t1, t2 = t2, t1
+print("After swapping" )
+print("First tuple")  
+print(t1)  
+print("Second tuple")  
+print(t2)
