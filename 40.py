2e09a8d59741df5b7df934da1a8c0fdeaeb7010b
Shatmanaya <sanjaykmr157@outlook.com>
2021-01-11 11:35:14 +0530

    solved 40

---
+++ 40.py
@@ -0,0 +1,11 @@
+# Program to input "n" numbers and store it in a tuple and find maximum and minimum values in the tuple
+t = tuple()
+n = int(input('enter number of values '))
+
+for i in range(n):
+    values = int(input("enter values "))
+    t = t + (values,)
+
+print("maximum value in the tuple ",max(t))
+print("minimum value in the tuple ",min(t)
+)
